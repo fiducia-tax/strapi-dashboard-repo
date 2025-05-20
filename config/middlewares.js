a73046df4d887eeefc25e6b0287fe3d65470216e
fiducia-tax <admin@fiduciatax.com>
2025-05-20 03:14:29 -0400

    inital setup

---
+++ config/middlewares.js
@@ -0,0 +1,11 @@
+module.exports = [
+  'strapi::logger',
+  'strapi::errors',
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
